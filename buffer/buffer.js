5b4b58c954f864a49a4ebc5130faf5daba8edd8d
Femy Catherine Joseph <femy.123@gmail.com>
2017-02-05 12:01:54 -0500

    Update buffer.js

--- buffer/buffer.js
+++ buffer/buffer.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 fs.readFile('/var/www/index.html', function(err, fileContent) {
 if (err) {
-throw err;
+throw err;f
 }
 console.log('file content', fileContent.toString());
 });
